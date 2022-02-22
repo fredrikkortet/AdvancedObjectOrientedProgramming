ac9260b6a4b70d3e26df8b62d1a005f133f7e849
fredrikkortet <49959669+fredrikkortet@users.noreply.github.com>
2022-02-22 09:02:42 +0000

    Add files via upload
    added the course

---
+++ labb3/task1/Element.java
@@ -0,0 +1,15 @@
+package labb3.task1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Element {
+
+	String name();
+
+}
